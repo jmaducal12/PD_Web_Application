ededa3f2731ff8982f83a6a4ebb160ea09972c51
John Mark Aducal <qjmsaducal@tip.edu.ph>
2024-07-27 17:20:18 +0800

    PD WEB APPLICATION

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home/home.js';
+import Top10Earthquakes from './TopEarthquakes/Top10Earthquakes.js';
+import Casualties from './Casualties/Casualties.js';
+import Shakertable from './Shakertable/Shakertable.js';
+import Output from './Output/Output.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/top-10-earthquakes" component={Top10Earthquakes} />
+        <Route path="/casualties" component={Casualties} />
+        <Route path="/shaker-table" component={Shakertable} />
+        <Route path="/output" component={Output} />
+      </Switch>
+    </Router>
   );
 }
 
